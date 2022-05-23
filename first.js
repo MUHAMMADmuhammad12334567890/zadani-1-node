af79ae4143623ffdaf2b05d5a272c9a25bc150c6
AgReSSoR <samahkinets@gmail.com>
2022-05-23 13:05:12 +0300

    zadany 1 node

---
+++ first.js
@@ -0,0 +1,11 @@
+
+let a = 5
+ let f = 7
+
+  const b = function () {
+    return a + f
+}
+// export {a, f, b};
+
+export default {a, f, b};
+
---
+++ index2.js
@@ -0,0 +1,7 @@
+import abc from "./first.js";
+
+console.log(abc)
+
+// import {a, f, b} from "./first.js";
+
+// console.log(a, f, b)
